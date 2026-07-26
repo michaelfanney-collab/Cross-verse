f1b53126dd4cd437dfb98161b410905f748ae603
michaelfanney-collab <michaelfanney@gmail.com>
2026-07-26 14:48:27 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -6,6 +6,19 @@ const supabase = window.supabase.createClient(
 
   SUPABASE_URL,
 
+  SUPABASE_const SUPABASE_URL = "https://zmjjxsxmutuggwmagiam.supabase.co";
+
+const SUPABASE_KEY = "";sb_publishable_cWnTJrz6JRKeP8VB_Pmoag_5dcEyIP9"
+
+console.log("Supabase loaded:", window.supabase);
+
+const supabase = window.supabase.createClient(
+
+  SUPABASE_URL,
+
   SUPABASE_KEY
 
 );
+
+window.supabaseClient = supabase;
+
