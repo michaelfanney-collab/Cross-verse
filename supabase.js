5bf512296f7be1ecb8da285514aa028c6cb249a7
michaelfanney-collab <michaelfanney@gmail.com>
2026-07-26 06:24:35 -0400

    Create supabase.js
    New

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL = "https://zmjjxsxmutuggwmagiam.supabase.co";
+
+const SUPABASE_KEY = "_sb_publishable_"
+
+const supabase = window.supabase.createClient(
+
+  SUPABASE_URL,
+
+  SUPABASE_KEY
+
+);
